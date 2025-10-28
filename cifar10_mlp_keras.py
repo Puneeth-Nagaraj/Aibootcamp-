3ec7c53c6f16ea85d325371f192149509bf3595e
Vandanaa-H <ullasn387@gmail.com>
2025-10-28 16:10:53 +0530

    initial cifar10

---
+++ cifar10_mlp_keras.py
@@ -0,0 +1,9 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense,Flatten
+from keras.datasets import cifar10
+from keras.utils import to_categorical
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+
+#
